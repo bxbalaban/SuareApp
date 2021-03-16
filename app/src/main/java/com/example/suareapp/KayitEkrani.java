f870fd087997b92dc80a91b8a32f1e62d6eee54b
bxbalaban <balabanbsr@gmail.com>
2021-03-17 02:24:55 +0300

    hatalar giderildi

--- app/src/main/java/com/example/suareapp/KayitEkrani.java
+++ app/src/main/java/com/example/suareapp/KayitEkrani.java
@@ -11,28 +11,24 @@ import android.widget.TextView;
 import org.w3c.dom.Text;
 
 public class KayitEkrani extends AppCompatActivity {
-private Button button3;
-public TextView textView3_adsoyad;
-public TextView editTextPhone;
+private Button get_otp;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kayit_ekrani);
 
-        Button button3=(Button)findViewById(R.id.button3);
-        TextView textView3_adsoyad=(TextView)findViewById(R.id.textView3_adsoyad);
-        TextView editTextPhone=(TextView)findViewById(R.id.editTextPhone);
+        Button get_otp=(Button)findViewById(R.id.get_otp);
 
-        button3.setOnClickListener(new View.OnClickListener() {
+        get_otp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent topage=new Intent(KayitEkrani.this, AnaEkran.class);
                 startActivity(topage);
             }
         });
-        String name=textView3_adsoyad.toString();
-        String phone=editTextPhone.toString();
+
 
 
     }
