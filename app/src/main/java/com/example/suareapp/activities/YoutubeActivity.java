923c0d61f0fd4967ddd798690934e382ba4ce979
bxbalaban <balabanbsr@gmail.com>
2021-09-12 17:39:17 +0300

    Fitness activity finished

--- app/src/main/java/com/example/suareapp/activities/YoutubeActivity.java
+++ app/src/main/java/com/example/suareapp/activities/YoutubeActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.webkit.WebSettings;
@@ -11,14 +12,16 @@ import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.MediaController;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.VideoView;
 
 import com.example.suareapp.R;
 
 public class YoutubeActivity extends AppCompatActivity  {
     String level;
-    WebView mWebView;
+
     String videoStr;
 
 
@@ -35,13 +38,7 @@ public class YoutubeActivity extends AppCompatActivity  {
         Button video_next=(Button)findViewById(R.id.video_next);
         Button video_before=(Button)findViewById(R.id.video_before);
 
-
-
-
-
-
-
-        mWebView = (WebView) findViewById(R.id.YoutubeView);
+        VideoView videoView = (VideoView) findViewById(R.id.YoutubeView);
 
 
         video_back.setOnClickListener(new View.OnClickListener() {
@@ -53,108 +50,19 @@ public class YoutubeActivity extends AppCompatActivity  {
         });
 
         if (level.equals("level1")){
-
-
-            //build your own src link with your video ID
-             videoStr = "<html><body><iframe width=\"100%\" height=\"100%\" src=\"https://www.youtube.com/embed/47yJ2XCRLZs\" frameborder=\"0\" allowfullscreen></iframe></body></html>";
-
-            mWebView.setWebViewClient(new WebViewClient() {
-                @Override
-                public boolean shouldOverrideUrlLoading(WebView view, String url) {
-                    return false;
-                }
-            });
-            WebSettings ws = mWebView.getSettings();
-            ws.setJavaScriptEnabled(true);
-            mWebView.loadData(videoStr, "text/html", "utf-8");
-
-
-
-            video_breathe.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-
-                     videoStr = "<html><body><iframe width=\"100%\" height=\"100%\" src=\"https://www.youtube.com/embed/47yJ2XCRLZs\" frameborder=\"0\" allowfullscreen></iframe></body></html>";
-
-                    mWebView.setWebViewClient(new WebViewClient() {
-                        @Override
-                        public boolean shouldOverrideUrlLoading(WebView view, String url) {
-                            return false;
-                        }
-                    });
-                    WebSettings ws = mWebView.getSettings();
-                    ws.setJavaScriptEnabled(true);
-                    mWebView.loadData(videoStr, "text/html", "utf-8");
-                }
-            });
-
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/watch?v=tnHYuOdzmj0&list=PL20Zn-5nPIPHvLPq5xJTTImOd0qeNd9rW"));
+            startActivity(intent);
 
         }
         else if (level.equals("level2")){
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/watch?v=tnHYuOdzmj0&list=PL20Zn-5nPIPHvLPq5xJTTImOd0qeNd9rW"));
+            startActivity(intent);
 
-            videoStr = "<html><body><iframe width=\"100%\" height=\"100%\" src=\"https://www.youtube.com/embed/47yJ2XCRLZs\" frameborder=\"0\" allowfullscreen></iframe></body></html>";
-
-            mWebView.setWebViewClient(new WebViewClient() {
-                @Override
-                public boolean shouldOverrideUrlLoading(WebView view, String url) {
-                    return false;
-                }
-            });
-            WebSettings ws = mWebView.getSettings();
-            ws.setJavaScriptEnabled(true);
-            mWebView.loadData(videoStr, "text/html", "utf-8");
-
-            video_breathe.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-
-                    //build your own src link with your video ID
-                     videoStr = "<html><body><iframe width=\"100%\" height=\"100%\" src=\"https://www.youtube.com/embed/De6rFH8oKsI\" frameborder=\"0\" allowfullscreen></iframe></body></html>";
-
-                    mWebView.setWebViewClient(new WebViewClient() {
-                        @Override
-                        public boolean shouldOverrideUrlLoading(WebView view, String url) {
-                            return false;
-                        }
-                    });
-                    WebSettings ws = mWebView.getSettings();
-                    ws.setJavaScriptEnabled(true);
-                    mWebView.loadData(videoStr, "text/html", "utf-8");
-
-                }
-            });
         }
         else if (level.equals("level3")){
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/watch?v=tnHYuOdzmj0&list=PL20Zn-5nPIPHvLPq5xJTTImOd0qeNd9rW"));
+            startActivity(intent);
 
-            videoStr = "<html><body><iframe width=\"100%\" height=\"100%\" src=\"https://www.youtube.com/embed/47yJ2XCRLZs\" frameborder=\"0\" allowfullscreen></iframe></body></html>";
-
-            mWebView.setWebViewClient(new WebViewClient() {
-                @Override
-                public boolean shouldOverrideUrlLoading(WebView view, String url) {
-                    return false;
-                }
-            });
-            WebSettings ws = mWebView.getSettings();
-            ws.setJavaScriptEnabled(true);
-            mWebView.loadData(videoStr, "text/html", "utf-8");
-
-            video_breathe.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    //build your own src link with your video ID
-                     videoStr = "<html><body><iframe width=\"100%\" height=\"100%\" src=\"https://www.youtube.com/embed/h8_dFMwFA4A\" frameborder=\"0\" allowfullscreen></iframe></body></html>";
-
-                    mWebView.setWebViewClient(new WebViewClient() {
-                        @Override
-                        public boolean shouldOverrideUrlLoading(WebView view, String url) {
-                            return false;
-                        }
-                    });
-                    WebSettings ws = mWebView.getSettings();
-                    ws.setJavaScriptEnabled(true);
-                    mWebView.loadData(videoStr, "text/html", "utf-8");
-                }
-            });
         }
         else{
             Toast.makeText(this, "Hata", Toast.LENGTH_SHORT).show();
@@ -175,21 +83,7 @@ public class YoutubeActivity extends AppCompatActivity  {
         ws.setJavaScriptEnabled(true);
         mWebView.loadData(videoStr, "text/html", "utf-8");*/
     }
-    public void onCompletion(WebView mp) {
-        finish(); //This will end the current activity
-    }
-    public void pause(){
-        //NOT videoview.pause(); Needn't save Stop position
-        if (mp != null){
-            mp.pause();
-        }
-    }
 
-    public void resume(){
-        //NOT videoview.resume();
-        if (mp != null){
-            mp.start(); //Video will begin where it stopped
-        }
-    }
+
 
 }
